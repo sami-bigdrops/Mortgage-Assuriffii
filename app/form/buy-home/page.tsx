d3a80e3c1ef32a77d317056b28dcd2bc8764849a
sami-bigdrops <sami@bigdropsmarketing.com>
2025-11-25 19:06:36 +0530

    Fixed folder structure

--- app/form/buy-home/page.tsx
+++
@@ -1,8 +0,0 @@
-'use client'
-
-import React from 'react'
-import BuyHome from '@/app/form/products/BuyHome'
-
-export default function BuyHomePage() {
-  return <BuyHome />
-}
--- app/form/refinance/page.tsx
+++
@@ -1,8 +0,0 @@
-'use client'
-
-import React from 'react'
-import RefinanceWrapper from '@/app/form/products/Refinance'
-
-export default function RefinancePage() {
-  return <RefinanceWrapper />
-}
